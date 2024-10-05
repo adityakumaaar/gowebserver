fac5c63b7c194d5e5415144275e0af11b396648d
Aditya Kumar <mail.adityakumaaar+code@gmail.com>
2024-10-06 00:59:24 +0530

    create and get feeds

---
+++ handler_feed.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/adityakumaaar/gowebserver/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+
+	type params struct {
+		Name string `json:"name"`
+		URL  string `json"url"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	param := params{}
+	err := decoder.Decode(&param)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error in parsing JSON: %v", err))
+		return
+	}
+
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      param.Name,
+		Url:       param.URL,
+		UserID:    user.ID,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error in creating Feed Entry %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, feed)
+}
+
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error in getting feeds %v", err))
+		return
+	}
+	respondWithJSON(w, 200, feeds)
+}
--- main.go
+++ main.go
@@ -55,6 +55,8 @@ func main() {
 	v1Router.Get("/err", handlerError)
 	v1Router.Post("/users", apiCfg.handlerCreateUsers)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 
 	router.Mount("/v1", v1Router)
 
